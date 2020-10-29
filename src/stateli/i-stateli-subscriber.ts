4f8f868041fd4f461964dd35c96002d3aac18f5f
Josh Walters <josh.walters@outlook.com>
2020-10-29 21:55:31 +0000

    removed unused interface

--- src/stateli/i-stateli-subscriber.ts
+++
@@ -1,5 +0,0 @@
-import { IStateliObservable } from './i-stateli-observable';
-
-export interface IStateliSubscriber<RootState, State> {
-  next: (observable?: IStateliObservable<RootState, State>) => void;
-}
